c77211ce4b25d29c948c61389215d4b9c5be2899
Gabrieliusv <gabrielius.svi@gmail.com>
2019-07-22 17:46:31 +0300

    Changed Footer.js, About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,7 +6,7 @@ class About extends Component {
 
         return (
 
-            <div className="container mt-4">
+            <div className="min-height container mt-4">
                 <h1 className="text-center">About</h1>
                 <div className="row m-3 p-2">
 
@@ -16,6 +16,7 @@ class About extends Component {
         Nunc tincidunt magna quis imperdiet tempus. Aliquam hendrerit aliquet tempor. Cras eu urna ac felis efficitur rhoncus. Curabitur tortor est, luctus id mauris eget, maximus convallis sem. Aliquam sit amet finibus tortor. Vivamus leo dui, convallis vitae luctus eget, feugiat et diam. Donec quis mi condimentum, dapibus mi sit amet, consequat odio. Maecenas quis diam ut nibh condimentum rhoncus id cursus erat. Curabitur placerat risus leo, cursus egestas velit faucibus vitae. Suspendisse vehicula feugiat ante vel malesuada. Phasellus id arcu auctor, condimentum mauris a, tempor ante. Nulla facilisi. Nam aliquam dictum nisl, ac vulputate lacus convallis sit amet.
         In laoreet lobortis augue. Suspendisse posuere bibendum enim, id lacinia sapien tincidunt vitae.
                 </p>
+                    <p>Proin eu tincidunt libero, a molestie leo. Vivamus tempor magna erat, non commodo nulla venenatis id. Quisque vitae diam at ligula malesuada sagittis. Aliquam erat volutpat. Suspendisse faucibus nunc nec diam dapibus, ac posuere velit accumsan. Vivamus aliquet lorem eget vulputate ornare. Nunc accumsan diam ut urna posuere vestibulum. Sed posuere sagittis est, non blandit lacus sagittis at. Proin at nisi nulla. Vestibulum lacus ante, fringilla vel mauris vel, euismod hendrerit arcu. Suspendisse finibus nec nisl sodales dignissim. Praesent vulputate lobortis pretium. Donec augue elit, molestie sit amet lacus in, fermentum vestibulum massa. Nulla congue metus est, eu iaculis erat pellentesque ut. Aenean ac ipsum congue, cursus quam in, tempor enim. Sed tincidunt purus non elementum fringilla.</p>
                 </div>
             </div>
         )
